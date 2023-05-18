2d30c05b073e95682af96186a1b3a804ca20cde2
ameeraazli <ameera.azli@ytl.com>
2023-05-18 15:17:02 +0800

    displaying items in a grid - add ripple effect for android

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -25,8 +25,8 @@ const styles = StyleSheet.create({
     shadowColor: 'black',
     shadowOpacity: 0.25,
     shadowOffset: {width: 0, height: 2},
-    shadowRadius: 8
-
+    shadowRadius: 8,
+    overflow: 'hidden',
   },
   button: {
     flex: 1
