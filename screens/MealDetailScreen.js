e333a5ba606f81172d907a98eaf49687704057d6
ameeraazli <ameera.azli@ytl.com>
2023-05-22 17:13:37 +0800

    changing the button to use icon button (a customized button)

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,9 +1,10 @@
 import { useLayoutEffect } from 'react';
-import { Text, View, Image, StyleSheet, ScrollView, Button } from 'react-native';
+import { Text, View, Image, StyleSheet, ScrollView } from 'react-native';
 
 import MealDetails from '../components/MealDetails';
 import Subtitle from '../components/MealDetail/Subtitle';
 import List from '../components/MealDetail/List';
+import IconButton from '../components/IconButton';
 import { MEALS } from '../data/dummy-data';
 
 function MealDetailScreen({route, navigation}) {
@@ -18,7 +19,13 @@ function MealDetailScreen({route, navigation}) {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
-        return <Button title='Tap me' onPress={headerButtonPressHandler} />
+        return (
+          <IconButton
+            icon="star"
+            color="white"
+            onPress={headerButtonPressHandler}
+          />
+        );
       }
     });
   }, [navigation, headerButtonPressHandler]); //dependencies
