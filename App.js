6bb9493ddcf38717bf5fa76db21e2473831abd4e
ameeraazli <ameera.azli@ytl.com>
2023-05-23 16:06:28 +0800

    fix on react-navigation issue

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return <Drawer.Navigator>
+    <Drawer.Screen name="Categories" component={CategoriesScreen} />
+  </Drawer.Navigator>
+}
 
 export default function App() {
   return (
@@ -21,8 +29,8 @@ export default function App() {
             contentStyle: { backgroundColor: '#3f2f25' }
         }}>
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
               title: 'All Categories'
             }}
